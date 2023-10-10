4f8c0beda89e54e86fc63a5ad6290465ad9c38f7
waleed <oualidchaouati3@gmail.com>
2023-10-11 01:05:30 +0200

    done

--- src/Components/Afficheur/Afficheur.jsx
+++ src/Components/Afficheur/Afficheur.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./afficheur.styles.scss";
+// import "./afficheur.styles.scss";
 export default function Afficher({ children, className = null }) {
   return <div className={`afficheur ${className}`}> {children}</div>;
 }
